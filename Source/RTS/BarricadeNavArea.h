9f80781cd3acf41e1f913d55ccda5d5652013819
chbeeb23 <chbeeb23@gmail.com>
2019-03-03 17:14:53 +0200

    Initial commit

---
+++ Source/RTS/BarricadeNavArea.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/Navigation/NavAreas/NavArea.h"
+#include "BarricadeNavArea.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_API UBarricadeNavArea : public UNavArea
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
